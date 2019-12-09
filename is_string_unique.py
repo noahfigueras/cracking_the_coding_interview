f5955871b9fdb081005bb4fa28d9b653372d0515
Noah Figueras <33466101+noahfigueras@users.noreply.github.com>
2019-12-08 23:24:14 -0800

    Update is_string_unique.py

--- is_string_unique.py
+++ is_string_unique.py
@@ -1,6 +1,9 @@
-'''Implement an algorithm to determine if a string has all unique characters. What if you cannot use additional data structures ?'''
-import string
+'''Implement an algorithm to determine if a string has all unique characters. What if you cannot use additional data structures ?
 
+-------------
+First Attempt
+-------------'''
+  
 word = 'pacolteriA'
 
 def isUnique(m,word):
@@ -20,3 +23,27 @@ if isuniqueChar(word):
   print('Sorry, this word does not have all unique characters')
 else:
   print('String with all unique characters')
+
+  '''In my first attempt I solved the problem through bruteforce with a O(n2) algorithm
+  
+  --------------
+  Second Attempt
+  --------------'''
+  
+word = str(input())
+
+def isUniqueChar2(word):
+  if len(word) > 256:
+    return False
+  char_at = [False] * 256
+  for letter in word:
+    value = ord(letter)
+    if char_at[value]:
+      return False
+    char_at[value] = True
+    print(char_at)
+  return True
+
+print(isUniqueChar2(word))
+
+''' After my first attempt I did a search on the boolean Data Structures and learn a new way to store strings'''
