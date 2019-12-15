5faa0182603a8d5240d188a491e347e46444337e
Noah Figueras <33466101+noahfigueras@users.noreply.github.com>
2019-12-14 17:59:58 -0800

    Update removeDuplicates.py

--- removeDuplicates.py
+++ removeDuplicates.py
@@ -8,7 +8,7 @@ class DoubleLinkedList:
     self.head = head
   
   class Node:
-    def __init__(self, data=None,next_node=None):
+    def __init__(self, data=None,next_node=None,prev=None):
       self.data = data
       self.next = next_node
       
@@ -19,28 +19,29 @@ class DoubleLinkedList:
       n = n.next_node
     print(n.data)
 
-  def insert(self, data):
+  def insertionSort(self, data):
     new_node = self.Node(data)
-    new_node.next_node = self.head
-    self.head = new_node
+    curr_node = self.Node()
+    if (self.head == None) or (new_node.data < self.head.data):
+        new_node.next_node = self.head
+        self.head = new_node
+    else:
+      while curr_node.next_node != None:   
+        if new_node.data > curr_node.data:
+          curr_node = curr_node.next_node
+        elif curr_node == new_node:
+          print('Error: Node already exists')
+          return
+        new_node.prev = curr_node
+        new_node.next_node = curr_node.next_node.next_node
+        curr_node.next_node = new_node
+    
     print('New node inserted',new_node.data)
-
-  def sort(self):
-    n = self.head
-    tmp = self.Node
-
-    while n.next_node != None:
-      if n > n.next_node:
-        tmp = n.next_node
-        n.next_node = n
-        n = 
-
-      
-
-
+        
+#ACTION
 List = DoubleLinkedList()
 
 for x in range(6):
-  List.insert(random.randrange(0, 50, 3))
+  List.insertionSort(random.randrange(0, 50, 3))
 
 List.printNodes()
